f0b5cfa3d133ffe1a9546ef62a763502207e41f3
Donny-zs <55076088+Donny-zs@users.noreply.github.com>
2025-02-22 23:04:55 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 local component = require("component")
 local computer = require("computer")
+local os = require("os")
 local redstone = component.redstone
 local transposer = component.transposer
+local sleep = os.sleep
 
 local outputChest = 1
 local storage = 2
@@ -9,4 +11,8 @@ local vacoomChest = 4
 
 --transferItem(sourceSide:number, sinkSide:number, count:number, sourceSlot:number, sinkSlot:number):number
 --compareStacks(side:number, slotA:number, slotB:number, checkNBT:boolean=false):boolean
+
+function checkFluid()
+  return redstone.getInput(2) != 0 -- пример для стороны 2
+end
 
