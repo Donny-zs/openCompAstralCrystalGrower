94442e748ef2b5d9fad94ff36bce77a34bd96845
Donny-zs <55076088+Donny-zs@users.noreply.github.com>
2025-02-22 22:42:51 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+local component = require("component")
+local computer = require("computer")
+local redstone = component.redstone
+local transposer = component.transposer
+
+local outputChest = 1
+local storage = 2
+local vacoomChest = 4
+
+--transferItem(sourceSide:number, sinkSide:number, count:number, sourceSlot:number, sinkSlot:number):number
+--compareStacks(side:number, slotA:number, slotB:number, checkNBT:boolean=false):boolean
+
