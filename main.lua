b0423e7b716b7d8136930fea8697e077809596c1
Donny-zs <55076088+Donny-zs@users.noreply.github.com>
2025-02-23 09:30:42 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,11 +8,12 @@ local sleep = os.sleep
 local outputChest = 1
 local storage = 2
 local vacoomChest = 4
+local vacoomChestSize = component.transposer.getInventorySize(vacoomChest)
 
 --transferItem(sourceSide:number, sinkSide:number, count:number, sourceSlot:number, sinkSlot:number):number
 --compareStacks(side:number, slotA:number, slotB:number, checkNBT:boolean=false):boolean
 
-function checkFluid()
-  return redstone.getInput(2) != 0 -- пример для стороны 2
+function checkFluid() --Возвращает true если жидкость присутствует
+  return redstone.getInput(2) != 0 
 end
 
